c27c84a00eed3eafadf7bcaaf752d4e43bfd293e
Haroen Viaene <hello@haroen.me>
2024-07-18 16:22:00 +0200

    try config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({
+const config = defineConfig({
   base: './',
   define: { 'process.env.NODE_ENV': '"production"' },
   optimizeDeps: {
@@ -9,3 +9,5 @@ export default defineConfig({
   },
   plugins: [react()],
 });
+
+export default config;
