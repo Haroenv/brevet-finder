a0d4165882212a2bf02efbadd5ef1a1303382f25
Haroen Viaene <hello@haroen.me>
2024-07-18 19:32:28 +0200

    not always production

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 const config = defineConfig({
   base: './',
-  define: { 'process.env.NODE_ENV': '"production"' },
+  define: { 'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV) },
   optimizeDeps: {
     include: ['react/jsx-runtime'],
   },
