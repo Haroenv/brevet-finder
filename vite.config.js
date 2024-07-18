8b8dbea8237b585b54fa7ed4a3767110a52a429a
Haroen Viaene <hello@haroen.me>
2024-07-18 16:17:30 +0200

    node env

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,11 @@
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   base: './',
   define: { 'process.env.NODE_ENV': '"production"' },
+  optimizeDeps: {
+    include: ['react/jsx-runtime'],
+  },
+  plugins: [react()],
 });
