b1f2cfe65aa67e903c04f32e8975f9ab46471678
Haroen Viaene <hello@haroen.me>
2024-07-18 16:11:27 +0200

    build things

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
-/** @type {import('vite').UserConfig} */
-export default {
+import { defineConfig } from 'vite';
+
+export default defineConfig({
   base: './',
   define: { 'process.env.NODE_ENV': '"production"' },
-};
+});
