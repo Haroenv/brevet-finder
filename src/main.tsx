147dc6a5e27c5352e7894b4872e980fab2255ece
Haroen Viaene <hello@haroen.me>
2024-07-19 09:28:44 +0200

    more footer

--- src/main.tsx
+++ src/main.tsx
@@ -209,7 +209,9 @@ root.render(
           <Results />
         </div>
       </div>
-      <footer>
+      <footer
+        style={{ textAlign: 'justify', maxWidth: '60ch', margin: '0 auto' }}
+      >
         <p>
           Made with 🚲 by <a href="https://haroen.me">Haroen Viaene</a>. Data
           sources:{' '}
@@ -219,6 +221,22 @@ root.render(
           , <a href="https://map.audax-club-parisien.com">ACP</a>. Code
           available on{' '}
           <a href="https://github.com/haroenv/brm-search">GitHub</a>.
+        </p>
+        <p>
+          A Brevet is a long-distance cycling event with as goal to move your
+          own boundaries, not a race. They are classified in different
+          distances, with as eventual goal the{' '}
+          <a href="https://www.paris-brest-paris.org">
+            Paris-Brest-Paris Randonneur
+          </a>{' '}
+          (1200km) event which is organised every four years. Every event has a
+          time limit, and you need to finish within that time limit to get a
+          validation, although usually this is fairly generous.
+        </p>
+        <p>
+          I invite you happily to find an event to participate in on this site,
+          which is frequently updated with information from different sources
+          (let me know if I'm missing any). Have a nice ride!
         </p>
       </footer>
     </div>
