6edd79d451fb62997363f1f22c2ee30e3dd58308
Haroen Viaene <hello@haroen.me>
2024-12-03 10:43:47 +0100

    ugh :facepalm:

--- src/admin/import.ts
+++ src/admin/import.ts
@@ -17,7 +17,9 @@ const data = (await Bun.file('brevets.json').json()) as Brevet[];
 
 const client = algoliasearch(ALGOLIA_APP, ALGOLIA_WRITE);
 
-await client.initIndex('brevets').partialUpdateObjects(data);
+await client
+  .initIndex('brevets')
+  .partialUpdateObjects(data, { createIfNotExists: true });
 
 if (GITHUB_STEP_SUMMARY) {
   Bun.write(GITHUB_STEP_SUMMARY, `Indexed ${data.length} brevets`);
