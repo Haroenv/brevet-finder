9adcbbde6ad333ec0c94cbb1f83de44044d3b87b
Haroen Viaene <hello@haroen.me>
2025-01-17 22:21:53 +0100

    fix nl

--- src/admin/export-netherlands.ts
+++ src/admin/export-netherlands.ts
@@ -1,140 +1,48 @@
+import { dateToNum } from '../date';
 import { Brevet } from '../types';
 import { checkOk } from './fetch-utils';
-import he from 'he';
 
 type Raw = {
-  id: string;
-  global_id: string;
-  global_id_lineage: string;
-  author: string;
-  status: string;
-  date: string;
-  date_utc: string;
-  modified: string;
-  modified_utc: string;
-  url: string;
-  rest_url: string;
-  title: string;
-  description: string;
-  excerpt: string;
-  slug: string;
-  image: boolean;
-  all_day: boolean;
-  start_date: string;
-  start_date_details: {
-    year: string;
-    month: string;
-    day: string;
-    hour: string;
-    minutes: string;
-    seconds: string;
-  };
-  end_date: string;
-  end_date_details: {
-    year: string;
-    month: string;
-    day: string;
-    hour: string;
-    minutes: string;
-    seconds: string;
-  };
-  utc_start_date: string;
-  utc_start_date_details: {
-    year: string;
-    month: string;
-    day: string;
-    hour: string;
-    minutes: string;
-    seconds: string;
-  };
-  utc_end_date: string;
-  utc_end_date_details: {
-    year: string;
-    month: string;
-    day: string;
-    hour: string;
-    minutes: string;
-    seconds: string;
-  };
-  timezone: string;
-  timezone_abbr: string;
-  cost: string;
-  cost_details: {
-    currency_symbol: string;
-    currency_code: string;
-    currency_position: string;
-    values: number[];
-  };
-  website: string;
-  show_map: boolean;
-  show_map_link: boolean;
-  hide_from_listings: boolean;
-  sticky: boolean;
-  featured: boolean;
-  categories: never[];
-  tags: { name: '200' | '2024' | 'BRM' }[];
-  venue: {
-    id: number;
-    author: string;
-    status: 'publish';
-    venue: string;
+  contact: { email: string; name: string };
+  distance: number;
+  entryFee: number;
+  from: {
+    Country: 'NL';
     address: string;
     city: string;
-    country: string;
-    zip: string;
+    region: string;
+    title: string;
   };
-  organizer: {
-    email: string;
-    organizer: string;
-  }[];
+  start: `2025-01-25T09:00:00Z[UTC]`;
+  title: string;
+  url: string;
 };
 
-async function fetchBrevets(
-  prev: Raw[] = [],
-  page: number = 1
-): Promise<Raw[]> {
-  const url = new URL(
-    'https://www.randonneurs.nl/wp-json/tribe/events/v1/events/'
-  );
+async function fetchBrevets(): Promise<Raw[]> {
+  const url = new URL('https://randonneurs.nl/api/v1/events');
   url.search = new URLSearchParams({
-    page: page.toString(),
-    per_page: '50',
-    start_date: '2024-01-01 00:00:00',
-    end_date: '2026-10-29 23:59:59',
-    status: 'publish',
+    from: new Date().toISOString().split('T')[0],
+    // TODO: fetch future events once endpoint accepts "until" parameter
+    // until: '2026-01-01',
   }).toString();
 
-  const { events = [], ...json }: { events: Raw[]; total_pages: number } =
-    await fetch(url)
-      .then(checkOk)
-      .then((res) => res.json());
+  const events: Raw[] = await fetch(url)
+    .then(checkOk)
+    .then((res) => res.json());
 
-  if (json.total_pages > page) {
-    return fetchBrevets([...prev, ...events], page + 1);
-  }
-
-  return [...prev, ...events];
+  return events;
 }
 
 function cleanBrevets(brevets: Raw[]): Brevet[] {
   return brevets.map((brevet) => {
-    const distance = parseInt(
-      brevet.tags.find(
-        (tag) => Number.isInteger(parseInt(tag.name)) && tag.name.endsWith('00')
-      )?.name || '0'
-    );
+    const date = brevet.start.split('T')[0].split('-').reverse().join('/');
+    const dateNumber = dateToNum(new Date(brevet.start.split('T')[0]));
+    const distance = brevet.distance;
     const country = 'The Netherlands';
-    const city = brevet.venue.city;
-    const title = he.decode(brevet.title);
-    const { year, month, day } = brevet.start_date_details;
-    const date = [day, month, year].join('/');
-    const dateNumber = parseInt([year, month, day].join(''), 10);
-    const { email: mail, organizer: club } = brevet.organizer[0] || {
-      email: '',
-      organizer: '',
-    };
-
-    brevet.description = brevet.description.slice(0, 10_000);
+    const city = brevet.from.city;
+    const title = brevet.title;
+    const mail = brevet.contact.email;
+    const club = 'Randonneurs NL';
 
     return {
       objectID: [date, distance, country, city].join('__'),
